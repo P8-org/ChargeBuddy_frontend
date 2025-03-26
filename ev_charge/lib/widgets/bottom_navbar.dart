8376129b0bdbac095fe607848490a5ece9a2dafe
Mathias Pallesen Munch <mpmunch@gmail.com>
2025-03-26 10:59:19 +0100

    fixed navbar index

--- ev_charge/lib/widgets/bottom_navbar.dart
+++ ev_charge/lib/widgets/bottom_navbar.dart
@@ -4,34 +4,46 @@ import 'package:go_router/go_router.dart';
 class BottomNavBar extends StatelessWidget {
   const BottomNavBar({Key? key}) : super(key: key);
 
+  static const List<NavItem> navItems = [
+    NavItem(route: '/', label: 'Home', icon: Icons.home),
+    NavItem(route: '/settings', label: 'Settings', icon: Icons.settings),
+  ];
+
   int _calculateSelectedIndex(BuildContext context) {
-    final String location = GoRouterState.of(context).uri.toString();
-    if (location.startsWith('/settings')) {
-      return 1;
+    final location = GoRouterState.of(context).uri.toString();
+    for (int i = 0; i < navItems.length; i++) {
+      if (location == navItems[i].route || location.startsWith('${navItems[i].route}/')) {
+        return i;
+      }
     }
     return 0;
   }
 
   @override
   Widget build(BuildContext context) {
-    final int selectedIndex = _calculateSelectedIndex(context);
+    final selectedIndex = _calculateSelectedIndex(context);
 
     return BottomNavigationBar(
       currentIndex: selectedIndex,
-      onTap: (index) {
-        if (index == 0) context.go('/');
-        if (index == 1) context.go('/settings');
-      },
-      items: const [
-        BottomNavigationBarItem(
-          icon: Icon(Icons.home),
-          label: 'Home',
-        ),
-        BottomNavigationBarItem(
-          icon: Icon(Icons.settings),
-          label: 'Settings',
-        ),
-      ],
+      onTap: (index) => context.go(navItems[index].route),
+      items: navItems.map((item) {
+        return BottomNavigationBarItem(
+          icon: Icon(item.icon),
+          label: item.label,
+        );
+      }).toList(),
     );
   }
+}
+
+class NavItem {
+  final String route;
+  final String label;
+  final IconData icon;
+
+  const NavItem({
+    required this.route,
+    required this.label,
+    required this.icon,
+  });
 }
