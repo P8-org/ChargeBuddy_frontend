7c0c9a916f97a3e0314777de8212ebed2e60385d
Mathias Pallesen Munch <mpmunch@gmail.com>
2025-03-26 10:05:36 +0100

    selected index

--- ev_charge/lib/widgets/bottom_navbar.dart
+++ ev_charge/lib/widgets/bottom_navbar.dart
@@ -4,21 +4,32 @@ import 'package:go_router/go_router.dart';
 class BottomNavBar extends StatelessWidget {
   const BottomNavBar({Key? key}) : super(key: key);
 
+  int _calculateSelectedIndex(BuildContext context) {
+    final String location = GoRouterState.of(context).uri.toString();
+    if (location.startsWith('/settings')) {
+      return 1;
+    }
+    return 0;
+  }
+
   @override
   Widget build(BuildContext context) {
+    final int selectedIndex = _calculateSelectedIndex(context);
+
     return BottomNavigationBar(
+      currentIndex: selectedIndex,
       onTap: (index) {
         if (index == 0) context.go('/');
         if (index == 1) context.go('/settings');
       },
       items: const [
         BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home'
+          icon: Icon(Icons.home),
+          label: 'Home',
         ),
         BottomNavigationBarItem(
-            icon: Icon(Icons.settings),
-            label: 'Settings'
+          icon: Icon(Icons.settings),
+          label: 'Settings',
         ),
       ],
     );
