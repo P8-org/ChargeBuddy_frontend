312c1b2f7a715c2b0e5495cb6cb7715ca560de14
razwaz <andpdud@gmail.com>
2025-03-28 14:05:55 +0100

    basic form validation and submission

--- ev_charge/lib/views/add-ev.dart
+++ ev_charge/lib/views/add-ev.dart
@@ -2,6 +2,73 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:ev_charge/widgets/bottom_navbar.dart';
 
+
+class EVForm extends StatefulWidget {
+  const EVForm({super.key});
+
+  @override
+  EVFormState createState() {
+    return EVFormState();
+  }
+}
+class EVFormState extends State<EVForm> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a `GlobalKey<FormState>`,
+  // not a GlobalKey<EVFormState>.
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController manufacturerController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: TextFormField(
+              decoration: const InputDecoration(
+                border: UnderlineInputBorder(),
+                labelText: 'Manufacturer',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return null;
+              },
+              controller: manufacturerController,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+            child: ElevatedButton(
+              onPressed: () {
+                // Validate returns true if the form is valid, or false otherwise.
+                if (_formKey.currentState!.validate()) {
+                  // If the form is valid, display a snackbar. In the real world,
+                  // you'd often call a server or save the information in a database.
+
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text('Added: "${manufacturerController.text}"')),
+                  );
+                  context.pop();
+                }
+                
+              },
+              child: const Text('Submit'),
+            ),
+          ),
+
+          // Add TextFormFields and ElevatedButton here.
+        ],
+      ),
+    );
+  }
+}
 class AddEv extends StatelessWidget {
   const AddEv({super.key});
 
@@ -12,13 +79,20 @@ class AddEv extends StatelessWidget {
         title: const Text('Add EV'),
         backgroundColor: Colors.blue,
       ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            context.pop();
-          },
-          child: const Text('Go Back'),
-        ),
+      body: Column (
+        children: [
+          Center(
+            child: ElevatedButton(
+              onPressed:  () {
+                context.pop();
+              },
+              child: const Text('Cancel'),
+            ),
+          ),
+          Center(
+            child: const EVForm()
+          ),
+        ]
       ),
       bottomNavigationBar: const BottomNavBar(),
     );
