a99421e3357ae5d3b08d93eaeffc259a7e631b1a
Mathias Pallesen Munch <mpmunch@gmail.com>
2025-03-28 12:58:56 +0100

    Renamed DB and removed init schema

--- ev_charge/lib/core/database.dart
+++ ev_charge/lib/core/database.dart
@@ -62,7 +62,7 @@ class AppDatabase extends _$AppDatabase {
 
   static QueryExecutor _openConnection() {
     return driftDatabase(
-      name: 'my_database',
+      name: 'chargeBuddy_database',
       native: const DriftNativeOptions(
         // By default, `driftDatabase` from `package:drift_flutter` stores the
         // database files in `getApplicationDocumentsDirectory()`.
