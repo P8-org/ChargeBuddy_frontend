91954645522e67db9c2802bd26241286cfacb022
razwaz <93257877+razwaz@users.noreply.github.com>
2025-04-23 13:51:12 +0200

    refactor ´EVForm´ to be independent of `HomePageVM` + formatting

--- ev_charge/lib/widgets/ev_form.dart
+++ ev_charge/lib/widgets/ev_form.dart
@@ -1,11 +1,7 @@
-import 'package:ev_charge/main.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:ev_charge/widgets/form_helper.dart';
-import 'package:ev_charge/database/database.dart';
 
-import 'package:ev_charge/viewmodels/ev_page_vm.dart';
 class EVForm extends StatefulWidget {
   const EVForm({super.key, this.id});
   final int? id;
@@ -15,6 +11,7 @@ class EVForm extends StatefulWidget {
     return EVFormState();
   }
 }
+
 class EVFormState extends State<EVForm> {
   // Create a global key that uniquely identifies the Form widget
   // and allows validation of the form.
@@ -29,14 +26,14 @@ class EVFormState extends State<EVForm> {
   TextEditingController userSetNameController = TextEditingController();
   TextEditingController maxChargingPowerController = TextEditingController();
 
-  late EvPageVM vm;
+  late formVM vm;
 
   @override
   void initState() {
     super.initState();
     if (widget.id != null) {
-      vm = EvPageVM(id: widget.id!);
-      vm.getEv();
+      vm = formVM();
+      vm.getEv(widget.id!);
     }
   }
 
@@ -51,11 +48,25 @@ class EVFormState extends State<EVForm> {
     if (widget.id == null) {
       return;
     }
-    modelNameController.text = vm.ev!.carModel.modelName;
-    modelYearController.text = vm.ev!.carModel.modelYear.toString();
-    userSetNameController.text = vm.ev!.userSetName;
-    batteryCapacityController.text = vm.ev!.carModel.batteryCapacity.toString();
-    maxChargingPowerController.text = vm.ev!.carModel.maxChargingPower.toString();
+    modelNameController.text = vm.ev.carModel.modelName;
+    modelYearController.text = vm.ev.carModel.modelYear.toString();
+    userSetNameController.text = vm.ev.userSetName;
+    batteryCapacityController.text = vm.ev.carModel.batteryCapacity.toString();
+    maxChargingPowerController.text = vm.ev.carModel.maxChargingPower.toString();
+  }
+
+  Text formSnackbarText() {
+    if (widget.id == null) {
+      return Text('Added: "${modelNameController.text}"');
+    }
+    return Text('Saved: "${modelNameController.text}"');
+  }
+
+  Text formSubmissionText() {
+    if (widget.id == null) {
+      return const Text('Add EV');
+    }
+    return const Text('Save Changes');
   }
 
   @override
@@ -64,59 +75,68 @@ class EVFormState extends State<EVForm> {
     return ListenableBuilder(
       listenable: getVM(),
       builder: (context, child) {
-      if (widget.id != null && vm.loading) {
-        return SizedBox();
-      }
-      initializeFields();
-      return Form(
-      key: _formKey,
-      child: ListView(
-        padding: const EdgeInsets.all(8),
-        children: <Widget>[
-          FormHelper.inputField("Model", modelNameController, FormHelper.stringValidator()),
-          FormHelper.inputField("Model Year", modelYearController, FormHelper.intValidator()),
-          FormHelper.inputField("Custom Name", userSetNameController, null), // TODO: Not used for anything currently
-          FormHelper.inputField("Battery Capacity (kWh)", batteryCapacityController, FormHelper.doubleValidator()),
-          FormHelper.inputField("Maximum Charging Power (kW)", maxChargingPowerController, FormHelper.doubleValidator()),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(foregroundColor: Colors.red),
-                  onPressed: () {
-                    context.pop();
-                  },
-                  child: const Text ('Cancel')),
-                ElevatedButton(
-              onPressed: () async {
-                
-                if (_formKey.currentState!.validate()) {
-                  final db = ProviderScope.containerOf(context).read(dbProvider);
+        if (widget.id != null && vm.loading) {
+          return SizedBox();
+        }
+        initializeFields();
+        return Form(
+          key: _formKey,
+          child: ListView(
+            padding: const EdgeInsets.all(8),
+            children: <Widget>[
+              FormHelper.inputField("Model", modelNameController, FormHelper.stringValidator()),
+              FormHelper.inputField("Model Year", modelYearController, FormHelper.intValidator()),
+              FormHelper.inputField("Custom Name", userSetNameController, null), // TODO: Not used for anything currently
+              FormHelper.inputField("Battery Capacity (kWh)", batteryCapacityController, FormHelper.doubleValidator()),
+              FormHelper.inputField("Maximum Charging Power (kW)", maxChargingPowerController, FormHelper.doubleValidator()),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 8,
+                  vertical: 16,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(foregroundColor: Colors.red),
+                      onPressed: () {context.pop();},
+                      child: const Text('Cancel'),
+                    ),
+                    ElevatedButton(
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          /* TODO Insert & Edit DB logic
+                          final db = ProviderScope.containerOf(context).read(dbProvider);
 
-                  await db.into(db.eVCarModels).insert(EVCarModelsCompanion.insert(
-                    modelName: modelNameController.text,
-                    modelYear: int.parse(modelYearController.text),
-                    batteryCapacity: double.parse(batteryCapacityController.text),
-                    maxChargingPower: double.parse(maxChargingPowerController.text),
-                  ));
-
-                  if (context.mounted){
-                    ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(content: Text('Added: "${modelNameController.text}"')),);
-                    context.pop(); 
-                  }
-                }                
-              },
-              child: const Text('Submit'),
-            ),
-              ],
-            )
+                          await db
+                              .into(db.eVCarModels)
+                              .insert(
+                                EVCarModelsCompanion.insert(
+                                  modelName: modelNameController.text,
+                                  modelYear: int.parse(modelYearController.text),
+                                  batteryCapacity: double.parse(batteryCapacityController.text),
+                                  maxChargingPower: double.parse(maxChargingPowerController.text),
+                                ),
+                              );
+                          */
+                          if (context.mounted) {
+                            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                                content: formSnackbarText(),
+                              ),
+                            );
+                            context.pop();
+                          }
+                        }
+                      },
+                      child: formSubmissionText(),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
-    );},
+        );
+      },
     );
   }
 }
--- ev_charge/lib/widgets/form_helper.dart
+++ ev_charge/lib/widgets/form_helper.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ev_charge/core/backend_service.dart';
+import 'package:ev_charge/core/models.dart';
 
 class FormHelper {
 
@@ -49,3 +51,35 @@ class FormHelper {
     };
   }
 }
+
+class formVM extends ChangeNotifier {
+  final BackendService _backendService;
+  late UserEV _ev;
+  bool _loading = false;
+  bool _isError = false;
+  String _errorMessage = "";
+
+  formVM({BackendService? backendService})
+    : _backendService = backendService ?? BackendService();
+
+  UserEV get ev => _ev;
+  bool get loading => _loading;
+  bool get isError => _isError;
+  String get errorMessage => _errorMessage;
+
+  Future<void> getEv(int id) async {
+    _loading = true;
+    notifyListeners();
+    try {
+      _ev = await _backendService.getEvById(id);
+      _isError = false;
+    } catch (e) {
+      _isError = true;
+      _errorMessage = e.toString();
+    } finally {
+      _loading = false;
+      notifyListeners();
+    }
+  }
+}
+
