63361e98e980ff146b94b1103f949185442faca8
APaludan <andreashp@outlook.com>
2025-04-28 12:22:23 +0200

    Update ev_schedulepage.dart

--- ev_charge/lib/views/ev_schedulepage.dart
+++ ev_charge/lib/views/ev_schedulepage.dart
@@ -13,8 +13,6 @@ class EvSchedulePage extends StatefulWidget {
 }
 
 class _EvSchedulePageState extends State<EvSchedulePage> {
-  double targetPercentage = 0.8;
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,83 +32,21 @@ class _EvSchedulePageState extends State<EvSchedulePage> {
             ),
           ),
 
-          // Expanded(
-          //   child: Container(
-          //     width: double.infinity,
-          //     padding: const EdgeInsets.all(16.0),
-          //     decoration: BoxDecoration(
-          //       color: Colors.grey,
-          //       borderRadius: BorderRadius.all(Radius.circular(16)),
-          //     ),
-          //     child: Center(
-          //       child: Text(
-          //         "Placeholder for future calendar/schedule implementation",
-          //         style: TextStyle(color: Colors.black, fontSize: 30),
-          //       ),
-          //     ),
-          //   ),
-          // ),
-          OutlinedButton(
-            onPressed: () async {
-              final TimeOfDay? deadline = await showDialog(
-                context: context,
-                builder: (context) {
-                  return Dialog(
-                    child: StatefulBuilder(
-                      builder: (context, setState) {
-                        return Column(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            TimePickerDialog(initialTime: TimeOfDay.now()),
-                            Text(
-                              "${(targetPercentage * 100).toStringAsFixed(0)}%",
-                            ),
-                            Slider(
-                              value: targetPercentage,
-                              onChanged: (val) {
-                                setState(() {
-                                  targetPercentage = val;
-                                });
-                              },
-                              max: 1,
-                              min: 0,
-                            ),
-                          ],
-                        );
-                      },
-                    ),
-                  );
-                },
-              );
-              if (deadline == null) return;
-              final bs = BackendService();
-              final now = DateTime.now();
-              final date =
-                  DateTime(
-                        now.year,
-                        now.month,
-                        now.day,
-                        deadline.hour,
-                        deadline.minute,
-                      ).isBefore(now)
-                      ? DateTime(
-                        now.year,
-                        now.month,
-                        now.day + 1,
-                        deadline.hour,
-                        deadline.minute,
-                      )
-                      : DateTime(
-                        now.year,
-                        now.month,
-                        now.day,
-                        deadline.hour,
-                        deadline.minute,
-                      );
-              await bs.postConstraint(widget.id, date, targetPercentage);
-              await DbManager.initialize();
-            },
-            child: Text("Update constraintst"),
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(16.0),
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.all(Radius.circular(16)),
+              ),
+              child: Center(
+                child: Text(
+                  "Placeholder for future calendar/schedule implementation",
+                  style: TextStyle(color: Colors.black, fontSize: 30),
+                ),
+              ),
+            ),
           ),
         ],
       ),
